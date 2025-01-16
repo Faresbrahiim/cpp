4ffb960dfd519947997fb19094d2a7fd42d053a6
ifares <hmsteven37@gmail.com>
2025-01-16 14:10:16 +0100

    cpp

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std ;
+
+
+int main() {
+cout << "hello world" << endl ;
+cout << "this is a test to check if the cpp works on zone computers" << endl;
+return 0 ;
+
+}
